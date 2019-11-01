6ba18e3c706b4457724c380da3d3aa80f2a0e7d0
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-01 09:18:14 +0000

    Endpoint to get project by id and middleware to validate id added

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -9,9 +9,30 @@ router.get('/', (req, res) => {
     })
     .catch(
         err => {
-            res.status(500).json({message: err.message})
+            res.status(500).json({message: "Something went terribly wrong trying to retrieve all projects: " + err.message})
         }
     )
 })
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.project)
+})
+
+//middleware
+function validateProjectId(req, res, next){
+    const { id } = req.params;
+    projects.get(id)
+    .then(project => {
+        if(project){
+            req.project = project;
+            next();
+        } else {
+            res.status(404).json({message: `Project with id of ${id} does not exist`})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: `Something went terribly wrong trying to retrieve project with id of ${id}: ${err.message}`})
+    })
+}
 
 module.exports = router;
